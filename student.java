cb5b141e455306cfb193397c4d59992063ded4d9
Jstowers1 <Lunkman728@gmail.com>
2023-08-31 11:47:32 -0500

    Added student, grading, teacher, work classes

--- student.java
+++ student.java
@@ -1,3 +1,13 @@
 public class student {
+    private static int id = 0;
+    private static String name = new String();
+    private static String[] courses = new String[8];
 
+    public static void student(int stuID, String stuName, String[] stuCourses) {
+        id = stuID;
+        name = stuName;
+        for (int i = 0; i <= 8; i++) {
+            courses[i] = stuCourses[i];
+        }
+    }
 }
--- teacher.java
+++ teacher.java
@@ -1,3 +1,11 @@
 public class teacher {
+    private static int id = 0;
+    private static String name = new String("");
+    private static String course = new String("");
 
+    public static void teacher(int teachId, String teachName, String teachCourse) {
+        id = teachId;
+        name = teachName;
+        course = teachCourse;
+    }
 }
