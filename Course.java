d40ce90c41e5471abea399c7fd259d1292ed9547
Aidan Underwood <aidanunderwood1@icloud.com>
2023-08-31 11:56:17 -0500

    great work

---
+++ Course.java
@@ -0,0 +1,9 @@
+public class Course {
+    private int courseID;
+    private String courseTitle, courseTeacher, courseStudents, courseAssignments,
+        courseDiscussions;
+
+    public Course() {
+
+    }
+}
