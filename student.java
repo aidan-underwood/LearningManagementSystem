1aee5eaa22f9e55508d3518b339268bb28318fbc
Jstowers1 <Lunkman728@gmail.com>
2023-08-31 12:01:51 -0500

    Co-authored-by: RJ Chaney <robertjchaney@users.noreply.github.com>

--- student.java
+++ student.java
@@ -1,9 +1,9 @@
-public class student {
+public class Student {
     private static int id = 0;
     private static String name = new String();
     private static String[] courses = new String[8];
 
-    public static void student(int stuID, String stuName, String[] stuCourses) {
+    public static void Student(int stuID, String stuName, String[] stuCourses) {
         id = stuID;
         name = stuName;
         for (int i = 0; i <= 8; i++) {
--- teacher.java
+++ teacher.java
@@ -1,11 +1,19 @@
-public class teacher {
+public class Teacher {
     private static int id = 0;
     private static String name = new String("");
     private static String course = new String("");
 
-    public static void teacher(int teachId, String teachName, String teachCourse) {
+    public static void Teacher(int teachId, String teachName, String teachCourse) {
         id = teachId;
         name = teachName;
         course = teachCourse;
+    }
+
+    public String getTeachName() {
+        return name;
+    }
+
+    public void setTeachName(String newName) {
+        this.name = newName;
     }
 }
