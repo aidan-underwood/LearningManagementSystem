0c890cc2f9c223e021e177e6a9f162fc92b4c167
Aidan Underwood <aidanunderwood1@icloud.com>
2023-09-01 10:52:00 -0500

    Revert "great work"
    This reverts commit d40ce90c41e5471abea399c7fd259d1292ed9547.

--- Course.java
+++
@@ -1,9 +0,0 @@
-public class Course {
-    private int courseID;
-    private String courseTitle, courseTeacher, courseStudents, courseAssignments,
-        courseDiscussions;
-
-    public Course() {
-
-    }
-}
